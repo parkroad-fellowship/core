b3a9b4632d59ab62333fb415b329438f04037432
Miller Adulu <milleradulu@gmail.com>
2024-06-04 01:29:53 +0300

    Set up mission type seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -37,6 +37,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             SchoolTermSeeder::class,
+            MissionTypeSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/MissionTypeSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MissionTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $missionType = [
+            'Weekend Challenge',
+            'Weekday Service',
+            'Sunday Service',
+            'Leadership Training',
+            'Guidance & Counselling Mission',
+            'Community Mission',
+            'Mission Training and Development (MTD)'
+        ];
+
+        foreach ($missionType as $type) {
+            \App\Models\MissionType::factory()->create([
+                'name' => $type
+            ]);
+        }
+    }
+}
