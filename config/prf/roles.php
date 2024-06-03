5d40028d6f4aedc4e02e1d91115c845b76e81ee3
Miller Adulu <milleradulu@gmail.com>
2024-06-03 13:44:28 +0300

    Install core packages

---
+++ config/prf/roles.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'roles' => [
+        'super admin' => [],
+        'chairperson' => [],
+        'vice chairperson' => [],
+        'organising secretary' => [],
+        'mission coordinator' => [],
+        'vice mission coordinator' => [],
+        'treasurer' => [],
+    ],
+];
