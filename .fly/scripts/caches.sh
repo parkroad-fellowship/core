f90f2d9ea4747dc4b18125f2b7d29ca355a4d344
Miller Adulu <milleradulu@gmail.com>
2024-06-08 02:00:55 +0300

    Setup fly.io scripts

---
+++ .fly/scripts/caches.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+/usr/bin/php /var/www/html/artisan config:cache --no-ansi -q
+/usr/bin/php /var/www/html/artisan route:cache --no-ansi -q
+/usr/bin/php /var/www/html/artisan view:cache --no-ansi -q
+
+/usr/bin/php /var/www/html/artisan migrate:fresh --seed --force --no-ansi -q
