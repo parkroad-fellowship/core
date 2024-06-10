56067d4a97943fdd99aa56a7946cd012add38c05
Miller Adulu <milleradulu@gmail.com>
2024-06-10 12:52:54 +0300

    Setup class groups dashboard

---
+++ app/Filament/Resources/ClassGroupResource/Pages/EditClassGroup.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Resources\ClassGroupResource\Pages;
+
+use App\Filament\Resources\ClassGroupResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditClassGroup extends EditRecord
+{
+    protected static string $resource = ClassGroupResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\ViewAction::make(),
+            Actions\DeleteAction::make(),
+            Actions\ForceDeleteAction::make(),
+            Actions\RestoreAction::make(),
+        ];
+    }
+}
