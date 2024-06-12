d3f9f78e6618ea4cb915d7a77c66b05c423d892f
tonnycoder001 <tonnykahihia@gmail.com>
2024-06-12 17:25:03 +0300

    Fix format

--- app/Filament/Resources/ChurchResource.php
+++ app/Filament/Resources/ChurchResource.php
@@ -110,6 +110,6 @@ class ChurchResource extends Resource
 
     public static function canAccess(): bool
     {
-        return auth()->user()->can('viewAny users');
+        return auth()->user()->can('viewAny church');
     }
 }
