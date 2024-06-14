2c168db98f91edd868d37fbd8dc68337ab604d4c
Miller Adulu <milleradulu@gmail.com>
2024-06-14 13:41:55 +0300

    Enforce user timezone to factor in UTC

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\TimePicker;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
@@ -24,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
         if (App::isProduction()) {
             URL::forceScheme('https');
         }
+
+        DateTimePicker::configureUsing(fn (DateTimePicker $component) => $component->timezone(auth()->user()?->timezone ?? config('app.timezone')));
+        TimePicker::configureUsing(fn (TimePicker $component) => $component->timezone(auth()->user()?->timezone ?? config('app.timezone')));
     }
 }
