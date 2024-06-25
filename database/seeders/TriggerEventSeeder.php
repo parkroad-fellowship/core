838fc6e0734cad0250b81edb9ca4973c19fee86f
Miller Adulu <milleradulu@gmail.com>
2024-06-25 23:54:06 +0300

    Fix formatting

--- database/seeders/TriggerEventSeeder.php
+++ database/seeders/TriggerEventSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Events\CoolBeans;
 use App\Models\Course;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class TriggerEventSeeder extends Seeder
