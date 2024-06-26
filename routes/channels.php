ba68bc30b72d5b16961b1afa21e2fd7099bf81e8
Miller Adulu <milleradulu@gmail.com>
2024-06-26 15:53:33 +0300

    Remove unnecessary log

--- routes/channels.php
+++ routes/channels.php
@@ -1,15 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
-use Illuminate\Support\Facades\Log;
 
 Broadcast::routes(['middleware' => 'auth:sanctum']);
 
 Broadcast::channel('App.Models.User.{ulid}', function ($user, $ulid) {
-    Log::info('Sending', [
-        'User' => $user,
-        'Auth ulid' => $ulid,
-    ]);
-
     return $user->ulid === $ulid;
 });
