dfc7dfce4efbca83d738a5ec9eaf84bdc30ba698
Miller Adulu <milleradulu@gmail.com>
2024-10-23 03:58:44 +0300

    Temporarily disable mission subscription seeding

--- database/seeders/MissionSeeder.php
+++ database/seeders/MissionSeeder.php
@@ -17,16 +17,16 @@ class MissionSeeder extends Seeder
     {
         $missions = Mission::factory()->count(10)->create();
 
-        $missions->each(function ($mission) {
-            // Attach members
-            $mission->missionSubscriptions()->createMany(
-                Member::inRandomOrder()->limit(rand(3, 10))->get()->map(function ($member) {
-                    return [
-                        'member_id' => $member->id,
-                        'status' => Arr::random(PRFMissionSubscriptionStatus::getValues()),
-                    ];
-                })->toArray()
-            );
-        });
+        // $missions->each(function ($mission) {
+        //     // Attach members
+        //     $mission->missionSubscriptions()->createMany(
+        //         Member::inRandomOrder()->limit(rand(3, 10))->get()->map(function ($member) {
+        //             return [
+        //                 'member_id' => $member->id,
+        //                 'status' => Arr::random(PRFMissionSubscriptionStatus::getValues()),
+        //             ];
+        //         })->toArray()
+        //     );
+        // });
     }
 }
