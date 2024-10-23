6611892e5562a5b5c7082df5166f9555deeb6570
Miller Adulu <milleradulu@gmail.com>
2024-10-23 03:12:53 +0300

    Update email seeding to seed parkroadfellowship.org emails

--- database/factories/MemberFactory.php
+++ database/factories/MemberFactory.php
@@ -7,6 +7,7 @@ use App\Models\Church;
 use App\Models\MaritalStatus;
 use App\Models\Profession;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Member>
@@ -28,7 +29,7 @@ class MemberFactory extends Factory
             'last_name' => $this->faker->lastName(),
             'postal_address' => $this->faker->address(),
             'phone_number' => $this->faker->phoneNumber(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'email' => Str::of($this->faker->unique()->safeEmail())->replace(['example.com', 'example.net', 'example.org'], 'parkroadfellowship.org'),
             'residence' => $this->faker->address(),
             'year_of_salvation' => $this->faker->numberBetween(1990, 2021),
             'church_volunteer' => $this->faker->boolean(),
