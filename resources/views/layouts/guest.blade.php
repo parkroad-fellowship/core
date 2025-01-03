e772f6c192ff025528e08e54a3a86fef4e5daa3b
Miller Adulu <milleradulu@gmail.com>
2025-01-04 00:01:26 +0300

    Update app assets

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,6 +11,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Logo -->
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
