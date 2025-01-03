a56d2d647d04d922ebc8cbd0cdb72710df163be5
Miller Adulu <milleradulu@gmail.com>
2025-01-03 19:30:32 +0300

    Update start script

--- .fly/scripts/caches.sh
+++ .fly/scripts/caches.sh
@@ -3,6 +3,7 @@
 /usr/bin/php /var/www/html/artisan config:cache --no-ansi -q
 /usr/bin/php /var/www/html/artisan route:cache --no-ansi -q
 /usr/bin/php /var/www/html/artisan view:cache --no-ansi -q
-/usr/bin/php /var/www/html/artisan migrate --force --no-ansi -q
-/usr/bin/php /var/www/html/artisan db:seed --class=RolesAndPermissionsSeeder --no-ansi -q
-/usr/bin/php /var/www/html/artisan db:seed --class=SpiritualYearSeeder --no-ansi -q
+/usr/bin/php /var/www/html/artisan migrate --seed --force --no-ansi -q
+# /usr/bin/php /var/www/html/artisan migrate --force --no-ansi -q
+# /usr/bin/php /var/www/html/artisan db:seed --class=RolesAndPermissionsSeeder --no-ansi -q
+# /usr/bin/php /var/www/html/artisan db:seed --class=SpiritualYearSeeder --no-ansi -q
