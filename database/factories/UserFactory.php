1772455650d66c0a3e63389c01eb1db791d9fcbc
Miller Adulu <milleradulu@gmail.com>
2025-01-03 19:47:48 +0300

    Fix failing test induced by email changes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'email' => Str::random(4) . '@parkroadfellowship.org',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'two_factor_secret' => null,
