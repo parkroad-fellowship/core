e142502f9e6583f07de02052843735676e1ad815
Miller Adulu <milleradulu@gmail.com>
2025-01-11 19:54:25 +0300

    Fix formatting

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Console\Commands\Mission\GenerateMissingWeatherRecommendationsCommand;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
 // Schedule missing weather recommendations for missions that are within 3 days to run daily at midnight
