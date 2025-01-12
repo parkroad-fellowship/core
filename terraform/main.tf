e7453dd4a69ad1a586748140695399f326ea49f8
Miller Adulu <milleradulu@gmail.com>
2025-01-12 04:57:23 +0300

    Attach public port

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,6 +37,7 @@ resource "azurerm_network_interface" "nic" {
     name                          = "prf-core-ip-configuration"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
