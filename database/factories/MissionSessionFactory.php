a8a6a1bfc3a9391b82fba8df3f6f1c11c334457c
Miller Adulu <milleradulu@gmail.com>
2025-01-13 07:49:57 +0300

    Setup the database layer for setting up mission sessions

---
+++ database/factories/MissionSessionFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ClassGroup;
+use App\Models\Member;
+use App\Models\Mission;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\MissionSession>
+ */
+class MissionSessionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'mission_id' => Mission::query()->inRandomOrder()->first()->getKey(),
+            'facilitator_id' => optional(Member::query()->inRandomOrder()->first())->getKey(),
+            'speaker_id' => optional(Member::query()->inRandomOrder()->first())->getKey(),
+            'class_group_id' => optional(ClassGroup::query()->inRandomOrder()->first())->getKey(),
+            'start_at' => $this->faker->dateTime(),
+            'end_at' => $this->faker->dateTime(),
+            'notes' => $this->faker->text(),
+        ];
+    }
+}
