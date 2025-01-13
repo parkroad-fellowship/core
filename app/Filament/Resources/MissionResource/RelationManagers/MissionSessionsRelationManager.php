160f4da342a46e393c0f0977d1360b66257a792f
Miller Adulu <milleradulu@gmail.com>
2025-01-13 08:30:30 +0300

    Fix formatting

--- app/Filament/Resources/MissionResource/RelationManagers/MissionSessionsRelationManager.php
+++ app/Filament/Resources/MissionResource/RelationManagers/MissionSessionsRelationManager.php
@@ -74,7 +74,7 @@ class MissionSessionsRelationManager extends RelationManager
                     Tables\Actions\RestoreBulkAction::make(),
                 ]),
             ])
-            ->modifyQueryUsing(fn(Builder $query) => $query->withoutGlobalScopes([
+            ->modifyQueryUsing(fn (Builder $query) => $query->withoutGlobalScopes([
                 SoftDeletingScope::class,
             ]));
     }
