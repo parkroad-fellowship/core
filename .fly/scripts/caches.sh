245e270f783ec0abfd2d8d00d05da3af7d0830de
Miller Adulu <milleradulu@gmail.com>
2025-01-29 13:58:50 +0300

    Enable verbose logs

--- .fly/scripts/caches.sh
+++ .fly/scripts/caches.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-/usr/bin/php /var/www/html/artisan config:cache --no-ansi -q
-/usr/bin/php /var/www/html/artisan route:cache --no-ansi -q
-/usr/bin/php /var/www/html/artisan view:cache --no-ansi -q
-/usr/bin/php /var/www/html/artisan migrate --force --no-ansi -q
-/usr/bin/php /var/www/html/artisan db:seed --class=ProductionSeeder --force --no-ansi -q
+/usr/bin/php /var/www/html/artisan config:cache --no-ansi
+/usr/bin/php /var/www/html/artisan route:cache --no-ansi
+/usr/bin/php /var/www/html/artisan view:cache --no-ansi
+/usr/bin/php /var/www/html/artisan migrate --force --no-ansi
+/usr/bin/php /var/www/html/artisan db:seed --class=ProductionSeeder --force --no-ansi
