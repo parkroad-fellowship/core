37dd3725eab9e5218835ba6a37782ddded588344
Miller Adulu <milleradulu@gmail.com>
2025-02-13 00:11:28 +0300

    Add a new line after every phrase so that it's not prose

--- app/Jobs/MissionSession/RetrieveTranscriptionJob.php
+++ app/Jobs/MissionSession/RetrieveTranscriptionJob.php
@@ -66,7 +66,7 @@ class RetrieveTranscriptionJob implements ShouldQueue
                     $combinedContent = '';
                     foreach ($transcription->json()['recognizedPhrases'] as $phrase) {
                         foreach ($phrase['nBest'] as $nBest) {
-                            $combinedContent .= $nBest['display'].' ';
+                            $combinedContent .= $nBest['display'].' \n';
                         }
                     }
 
