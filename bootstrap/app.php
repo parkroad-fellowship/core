2c4f88ef60728da7be9aefdaad42e65685bf86d4
Miller Adulu <milleradulu@gmail.com>
2025-02-14 01:55:12 +0300

    Update channel routing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,10 +7,10 @@ use Inspector\Laravel\Middleware\WebRequestMonitoring;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        channels: __DIR__.'/../routes/channels.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
+        // channels: __DIR__ . '/../routes/channels.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -20,11 +20,19 @@ return Application::configure(basePath: dirname(__DIR__))
             ->appendToGroup('web', WebRequestMonitoring::class)
             ->appendToGroup('api', WebRequestMonitoring::class);
     })
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->validateCsrfTokens(except: [
-            'broadcasting/auth',
-        ]);
-    })
+    // ->withMiddleware(function (Middleware $middleware) {
+    //     $middleware->validateCsrfTokens(except: [
+    //         'broadcasting/auth',
+    //     ]);
+    // })
+    ->withBroadcasting(
+        
+        __DIR__ . '/../routes/channels.php',
+        [
+            'prefix' => 'api',
+            'middleware' => ['auth:sanctum'],
+        ],
+    )
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
