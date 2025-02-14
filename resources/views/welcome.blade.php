41dd78075742011178685281ceb9736b53a40a49
Miller Adulu <milleradulu@gmail.com>
2025-02-14 11:35:19 +0300

    Center logo on the screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
-    <div class="pt-4 bg-gray-100">
-        <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
+    <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+        <div class="flex flex-col items-center">
             <div>
                 <x-authentication-card-logo />
             </div>
