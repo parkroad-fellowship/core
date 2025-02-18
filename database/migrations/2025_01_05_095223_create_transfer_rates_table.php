177bc80c70d3c56eb8553f7a0126b9eabeca86e7
Miller Adulu <milleradulu@gmail.com>
2025-02-18 10:00:24 +0300

    Fix bug with the transfer rates migration

--- database/migrations/2025_01_05_095223_create_transfer_rates_table.php
+++ database/migrations/2025_01_05_095223_create_transfer_rates_table.php
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('mpesa_rates');
+        Schema::dropIfExists('transfer_rates');
     }
 };
