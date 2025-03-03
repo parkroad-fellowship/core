1f9b2336ca292e3b108f28b35568e55524e535ad
Miller Adulu <milleradulu@gmail.com>
2025-03-03 17:51:13 +0300

    Fix formatting

--- database/migrations/2025_03_03_125834_add_payment_fields.php
+++ database/migrations/2025_03_03_125834_add_payment_fields.php
@@ -18,7 +18,6 @@ return new class extends Migration
                 'order_meta',
                 'order_tracking_id',
             ]);
-            
 
             $table->string('reference')->nullable()->unique();
             $table->string('access_code')->nullable()->unique();
