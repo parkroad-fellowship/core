8e690dedc41786f37fb8967854d09064100cd670
Miller Adulu <milleradulu@gmail.com>
2025-03-04 06:11:29 +0300

    Run commands only on one server

--- routes/console.php
+++ routes/console.php
@@ -7,12 +7,15 @@ use Illuminate\Support\Facades\Schedule;
 // Schedule missing weather recommendations for missions that are within 3 days to run daily at midnight
 Schedule::command(GenerateMissingWeatherRecommendationsCommand::class)
     ->dailyAt('00:00')
-    ->withoutOverlapping();
+    ->withoutOverlapping()
+    ->onOneServer();
 
 Schedule::command(\App\Console\Commands\PRFEvent\GenerateMissingWeatherRecommendationsCommand::class)
     ->dailyAt('00:00')
-    ->withoutOverlapping();
+    ->withoutOverlapping()
+    ->onOneServer();
 
 Schedule::command(CheckStatusCommand::class)
     ->everyThreeMinutes()
-    ->withoutOverlapping();
+    ->withoutOverlapping()
+    ->onOneServer();
