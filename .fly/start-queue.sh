0b28f70d4e6e6c95e9f1b7dad6842958ae135b8a
Miller Adulu <milleradulu@gmail.com>
2025-03-22 09:39:24 +0300

    Add queue backoff

--- .fly/start-queue.sh
+++ .fly/start-queue.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sleep 0.95 && exec /usr/bin/php /var/www/html/artisan queue:work --sleep=3 --tries=3
+sleep 0.95 && exec /usr/bin/php /var/www/html/artisan queue:work --sleep=3 --tries=3 --backoff=3
