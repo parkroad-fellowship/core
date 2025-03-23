e521eedc2c7c8f830542bca096606135980027b0
Miller Adulu <milleradulu@gmail.com>
2025-03-23 13:24:35 +0300

    Delegate the notification of lesson progress to the queue

--- app/Observers/LessonMemberObserver.php
+++ app/Observers/LessonMemberObserver.php
@@ -5,6 +5,7 @@ namespace App\Observers;
 use App\Enums\PRFCompletionStatus;
 use App\Events\LessonMember\Created;
 use App\Http\Resources\LessonModule\Resource;
+use App\Jobs\LessonMember\NotifyProgressJob;
 use App\Models\LessonMember;
 use App\Models\LessonModule;
 use App\Models\Member;
@@ -18,69 +19,7 @@ class LessonMemberObserver
      */
     public function created(LessonMember $lessonMember): void
     {
-        $memberModule = MemberModule::updateOrCreate(
-            [
-                'course_id' => $lessonMember->course_id,
-                'module_id' => $lessonMember->module_id,
-                'member_id' => $lessonMember->member_id,
-            ],
-            [
-                'course_id' => $lessonMember->course_id,
-                'module_id' => $lessonMember->module_id,
-                'member_id' => $lessonMember->member_id,
-            ],
-        );
-
-        $completedLessonsInModule = LessonMember::query()
-            ->where([
-                'course_id' => $lessonMember->course_id,
-                'module_id' => $lessonMember->module_id,
-                'member_id' => $lessonMember->member_id,
-                'completion_status' => PRFCompletionStatus::COMPLETE,
-            ])
-            ->count();
-
-        $lessonsInModule = LessonModule::query()
-            ->where('module_id', $lessonMember->module_id)
-            ->count();
-
-        $percentComplete = ($completedLessonsInModule / $lessonsInModule);
-
-        $memberModule->update(
-            [
-                'percent_complete' => $percentComplete * 100,
-                'completion_status' => match ($percentComplete) {
-                    1 => PRFCompletionStatus::COMPLETE,
-                    default => PRFCompletionStatus::INCOMPLETE,
-                },
-                'completed_at' => $percentComplete === 1 ? now() : null,
-            ],
-        );
-
-        $user = User::query()
-            ->where('id', Member::query()
-                ->where('id', $memberModule->member_id)
-                ->select('user_id')
-                ->limit(1))
-            ->firstOrFail();
-
-        $lessonMember->load(['course', 'module', 'lesson']);
-
-        $lessonModule = LessonModule::query()
-            ->where([
-                'lesson_id' => $lessonMember->lesson_id,
-                'module_id' => $lessonMember->module_id,
-            ])
-            ->first();
-
-        $lessonModule
-            ->load(['lesson', 'module'])
-            ->setRelation('lessonMember', $lessonMember);
-
-        Created::dispatch(
-            new Resource($lessonModule),
-            $user->ulid,
-        );
+        NotifyProgressJob::dispatch($lessonMember);
     }
 
     /**
