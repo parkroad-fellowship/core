63f93c4d11a1aca6d1932025702a648ac6edabdd
Miller Adulu <milleradulu@gmail.com>
2025-03-23 13:27:06 +0300

    Delegate the notification of module progress to a queue

--- app/Observers/MemberModuleObserver.php
+++ app/Observers/MemberModuleObserver.php
@@ -5,6 +5,7 @@ namespace App\Observers;
 use App\Enums\PRFCompletionStatus;
 use App\Events\MemberModule\Updated;
 use App\Http\Resources\CourseModule\Resource;
+use App\Jobs\MemberModule\NotifyProgressJob;
 use App\Models\CourseMember;
 use App\Models\CourseModule;
 use App\Models\Member;
@@ -35,61 +36,7 @@ class MemberModuleObserver
      */
     public function updated(MemberModule $memberModule): void
     {
-        $courseMember = CourseMember::query()
-            ->where([
-                'course_id' => $memberModule->course_id,
-                'member_id' => $memberModule->member_id,
-            ])
-            ->firstOrFail();
-
-        $completedModulesInCourse = MemberModule::query()
-            ->where([
-                'course_id' => $courseMember->course_id,
-                'member_id' => $courseMember->member_id,
-                'completion_status' => PRFCompletionStatus::COMPLETE,
-            ])
-            ->count();
-
-        $modulesInCourse = CourseModule::query()
-            ->where('course_id', $courseMember->course_id)
-            ->count();
-
-        $percentComplete = ($completedModulesInCourse / $modulesInCourse);
-
-        $courseMember->update(
-            [
-                'percent_complete' => $percentComplete * 100,
-                'completion_status' => match ($percentComplete) {
-                    1 => PRFCompletionStatus::COMPLETE,
-                    default => PRFCompletionStatus::INCOMPLETE,
-                },
-                'completed_at' => $percentComplete === 1 ? now() : null,
-            ],
-        );
-
-        $user = User::query()
-            ->where('id', Member::query()
-                ->where('id', $memberModule->member_id)
-                ->select('user_id')
-                ->limit(1))
-            ->firstOrFail();
-
-        $courseModule = CourseModule::query()
-            ->where([
-                'course_id' => $memberModule->course_id,
-                'module_id' => $memberModule->module_id,
-            ])
-            ->with([
-                'course.thumbnail',
-                'memberModule',
-                'module',
-            ])
-            ->firstOrFail();
-
-        Updated::dispatch(
-            new Resource($courseModule),
-            $user->ulid,
-        );
+        NotifyProgressJob::dispatch($memberModule);
     }
 
     /**
