118f0fad0b8c0f022f3e94034815d9dac9c27684
Miller Adulu <milleradulu@gmail.com>
2025-04-12 00:00:40 +0300

    Update email copy

--- app/Notifications/Mission/NewMissionNotification.php
+++ app/Notifications/Mission/NewMissionNotification.php
@@ -42,13 +42,14 @@ class NewMissionNotification extends Notification implements ShouldQueue
         return (new MailMessage)
             ->subject("New Mission: {$mission->school->name}")
             ->greeting("Hello {$notifiable->full_name},")
-            ->line("A new mission has been created for {$mission->school->name}.")
-            ->line("Type: {$mission->missionType->name}")
-            ->line("Start Date: {$mission->start_date->format('D, d-M-Y')} at {$mission->start_time}")
-            ->line("End Date: {$mission->end_date->format('D, d-M-Y')} at {$mission->end_time}")
-            ->line('Please visit the missions app to subscribe to this mission and to view more details.')
-            ->action('Google Play', 'https://play.google.com/store/apps/details?id=org.parkroadfellowship.app&hl=en')
-            ->line('Thank you for using our application!');
+            ->line("New mission for {$mission->school->name}:")
+            ->line("• Type: {$mission->missionType->name}")
+            ->line("• Dates: {$mission->start_date->format('d-M-Y')} to {$mission->end_date->format('d-M-Y')}")
+            ->line("• Times: {$mission->start_time} - {$mission->end_time}")
+            ->line('')
+            ->line('Subscribe in the app:')
+            ->action('Open App', 'https://play.google.com/store/apps/details?id=org.parkroadfellowship.app&hl=en')
+            ->line('Thank you!');
     }
 
     /**
