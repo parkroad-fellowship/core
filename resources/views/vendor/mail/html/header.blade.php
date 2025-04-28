a7f850a15515f2de582dda957726cc283110af75
Miller Adulu <milleradulu@gmail.com>
2025-04-28 18:42:40 +0300

    Update email logo to reduce email size and avoid truncation

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,8 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('landscape-logo.png'))) }}"
-                class="logo" alt="Parkroad Fellowship">
+            <img src="{{ url('landscape-logo.png') }}" class="logo" alt="Parkroad Fellowship">
         </a>
     </td>
 </tr>
