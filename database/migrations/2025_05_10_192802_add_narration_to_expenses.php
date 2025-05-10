427dd4ec6f5e75032a2ccd4c1d9770c70f408aa0
Miller Adulu <milleradulu@gmail.com>
2025-05-10 22:32:32 +0300

    Update the narration data type

--- database/migrations/2025_05_10_192802_add_narration_to_expenses.php
+++ database/migrations/2025_05_10_192802_add_narration_to_expenses.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('expenses', function (Blueprint $table) {
-            $table->string('narration')->nullable();
+            $table->longText('narration')->nullable();
         });
     }
 
