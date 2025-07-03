198de1ab8b4d66da7d770cb959b211834c11ae06
Miller Adulu <milleradulu@gmail.com>
2025-07-03 23:54:48 +0300

    Update https force check

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -39,7 +39,7 @@ class AppServiceProvider extends ServiceProvider
             return $user->hasRole('super admin');
         });
 
-        if (! App::isLocal()) {
+        if (! App::environment('local')) {
             URL::forceScheme('https');
         }
 
