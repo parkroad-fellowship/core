476e6963d50fcb62f1b5ce141a864369a1c4700a
Miller Adulu <milleradulu@gmail.com>
2025-07-04 20:22:09 +0300

    Fix trust proxies

--- Dockerfile
+++ Dockerfile
@@ -142,9 +142,7 @@ RUN composer install --optimize-autoloader --no-dev \
     && php artisan optimize:clear \
     && chown -R www-data:www-data /var/www/html \
     && echo "MAILTO=\"\"\n* * * * * www-data /usr/bin/php /var/www/html/artisan schedule:run" > /etc/cron.d/laravel \
-    && sed -i='' '/->withMiddleware(function (Middleware \$middleware) {/a\
-    \$middleware->trustProxies(at: "*");\
-    ' bootstrap/app.php; \ 
+    && sed -i 's|//|$middleware->trustProxies(at: "*");|' bootstrap/app.php \ 
     if [ -d .fly ]; then cp .fly/entrypoint.sh /entrypoint; chmod +x /entrypoint; fi;
 
 
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Inspector\Laravel\Middleware\WebRequestMonitoring;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,10 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // Inspector.dev middleware
-        $middleware
-            ->appendToGroup('web', WebRequestMonitoring::class)
-            ->appendToGroup('api', WebRequestMonitoring::class);
+        //
     })
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
