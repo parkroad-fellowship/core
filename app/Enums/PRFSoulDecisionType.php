a80e280969c3e8ef558e4ce1df481b7c754c9db7
Miller Adulu <milleradulu@gmail.com>
2025-07-10 17:54:52 +0300

    Add more soul decision categories

--- app/Enums/PRFSoulDecisionType.php
+++ app/Enums/PRFSoulDecisionType.php
@@ -8,13 +8,17 @@ enum PRFSoulDecisionType: int
 {
     case SALVATION = 1;
     case REDEDICATION = 2;
-    case OTHER = 3;
+    case CAMP = 3;
+    case PRAYER = 4;
+    case OTHER = 5;
 
     public static function getOptions(): array
     {
         return [
             self::SALVATION->value => 'Salvation',
             self::REDEDICATION->value => 'Rededication',
+            self::CAMP->value => 'Camp',
+            self::PRAYER->value => 'Prayer',
             self::OTHER->value => 'Other',
         ];
     }
@@ -24,6 +28,8 @@ enum PRFSoulDecisionType: int
         return [
             self::SALVATION->value => 'Salvation',
             self::REDEDICATION->value => 'Rededication',
+            self::CAMP->value => 'Camp',
+            self::PRAYER->value => 'Prayer',
             self::OTHER->value => 'Other',
 
         ];
@@ -34,6 +40,8 @@ enum PRFSoulDecisionType: int
         return match ($this) {
             self::SALVATION => 'Salvation',
             self::REDEDICATION => 'Rededication',
+            self::CAMP => 'Camp',
+            self::PRAYER => 'Prayer',
             self::OTHER => 'Other',
         };
     }
@@ -43,6 +51,8 @@ enum PRFSoulDecisionType: int
         return match ($this) {
             self::SALVATION => 'success',
             self::REDEDICATION => 'info',
+            self::CAMP => 'success',
+            self::PRAYER => 'info',
             self::OTHER => 'warning',
         };
     }
@@ -52,6 +62,8 @@ enum PRFSoulDecisionType: int
         return match ($this) {
             self::SALVATION => 'heroicon-o-check-circle',
             self::REDEDICATION => 'heroicon-o-refresh',
+            self::CAMP => 'heroicon-o-check-circle',
+            self::PRAYER => 'heroicon-o-check-circle',
             self::OTHER => 'heroicon-o-question-mark-circle',
         };
     }
@@ -73,6 +85,8 @@ enum PRFSoulDecisionType: int
         return match ($value) {
             self::SALVATION->value => self::SALVATION,
             self::REDEDICATION->value => self::REDEDICATION,
+            self::CAMP->value => self::CAMP,
+            self::PRAYER->value => self::PRAYER,
             self::OTHER->value => self::OTHER,
         };
     }
@@ -82,6 +96,8 @@ enum PRFSoulDecisionType: int
         return match ($enum) {
             self::SALVATION => self::SALVATION,
             self::REDEDICATION => self::REDEDICATION,
+            self::CAMP => self::CAMP,
+            self::PRAYER => self::PRAYER,
             self::OTHER => self::OTHER,
         };
     }
@@ -91,6 +107,8 @@ enum PRFSoulDecisionType: int
         return [
             self::SALVATION->value => self::SALVATION->getLabel(),
             self::REDEDICATION->value => self::REDEDICATION->getLabel(),
+            self::CAMP->value => self::CAMP->getLabel(),
+            self::PRAYER->value => self::PRAYER->getLabel(),
             self::OTHER->value => self::OTHER->getLabel(),
         ];
     }
@@ -100,6 +118,8 @@ enum PRFSoulDecisionType: int
         return [
             self::SALVATION->value,
             self::REDEDICATION->value,
+            self::CAMP->value,
+            self::PRAYER->value,
             self::OTHER->value,
         ];
     }
