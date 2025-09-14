ae88c90b10d5183bb338405f3dd20750425db43b
Miller Adulu <milleradulu@gmail.com>
2025-09-14 15:45:41 +0300

    Setup database layer to allow the recording of speakers

---
+++ app/Models/Speaker.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\HasUlid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Speaker extends Model
+{
+    /** @use HasFactory<\Database\Factories\SpeakerFactory> */
+    use HasFactory;
+
+    use HasUlid;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'ulid',
+        'name',
+        'title',
+        'bio',
+    ];
+}
