8b53f9801468fb20bea6e55af18a5e7df0f2df18
Miller Adulu <milleradulu@gmail.com>
2026-03-13 14:43:16 +0300

    Re-enable signature verification

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,16 +25,15 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     // API middleware groups
     ->withMiddleware(function (Middleware $middleware) {
-        // TODO: Enable reqeust signatures once we deploy the client changes
-        // $middleware->alias([
-        //     'verify.signature' => VerifyRequestSignature::class,
-        // ]);
+        $middleware->alias([
+            'verify.signature' => VerifyRequestSignature::class,
+        ]);
 
-        // $middleware->api(append: [
-        //     VerifyRequestSignature::class,
-        // ]);
+        $middleware->api(append: [
+            VerifyRequestSignature::class,
+        ]);
 
-        // $middleware->throttleApi('api');
+        $middleware->throttleApi('api');
     })
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
