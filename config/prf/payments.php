91371c789bffc99864553611dfb735c2aca56e89
Miller Adulu <milleradulu@gmail.com>
2026-04-30 21:34:07 +0300

    Automated sync from private repo

--- config/prf/payments.php
+++ config/prf/payments.php
@@ -5,6 +5,6 @@ return [
         'public_key' => env('PAYSTACK_PUBLIC_KEY'),
         'secret_key' => env('PAYSTACK_SECRET_KEY'),
         'base_url' => env('PAYSTACK_API_URL', 'https://api.paystack.co'),
-        'callback_url' => env('PAYSTACK_CALLBACK_URL', 'https://localhost/payments/success'),
+        'callback_url' => env('PAYSTACK_CALLBACK_URL', 'https://app.parkroadfellowship.org/payments/success'),
     ],
 ];
