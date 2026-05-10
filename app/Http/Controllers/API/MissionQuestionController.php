be936d65d8e1a42547b2ea324d150c6122163925
Miller Adulu <milleradulu@gmail.com>
2026-05-10 10:39:47 +0300

    Attach mission question audio responses

--- app/Http/Controllers/API/MissionQuestionController.php
+++ app/Http/Controllers/API/MissionQuestionController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\MissionQuestion\AttachMediaRequest;
 use App\Http\Requests\MissionQuestion\CreateRequest;
 use App\Http\Requests\MissionQuestion\UpdateRequest;
 use App\Http\Resources\MissionQuestion\Resource;
+use App\Jobs\Media\DeleteTemporaryFileJob;
 use App\Jobs\MissionQuestion\CreateJob;
 use App\Jobs\MissionQuestion\UpdateJob;
 use App\Models\MissionQuestion;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 use Spatie\QueryBuilder\QueryBuilder;
 
 class MissionQuestionController extends Controller
@@ -51,5 +59,87 @@ class MissionQuestionController extends Controller
             ->firstOrFail();
 
         return new Resource($missionQuestion);
+    }
+
+    public function getMedia(Request $request, string $ulid): AnonymousResourceCollection|JsonResponse
+    {
+        $collection = $request->query('collection');
+        $collections = $request->query('collections', [$collection]);
+
+        if (empty($collections)) {
+            return response()->json([
+                'message' => 'You must provide a collection',
+            ], 400);
+        }
+
+        // Handle both string and array formats
+        if (is_string($collections)) {
+            $collections = explode(',', $collections);
+        } else {
+            $collections = Arr::wrap($collections);
+        }
+
+        foreach ($collections as $collection) {
+            if (! in_array($collection, MissionQuestion::MEDIA_COLLECTIONS)) {
+                return response()->json([
+                    'message' => "Invalid collection: {$collection}",
+                ], 400);
+            }
+        }
+
+        $missionQuestion = MissionQuestion::query()
+            ->where('ulid', $ulid)
+            ->firstOrFail();
+
+        $media = collect();
+
+        foreach ($collections as $collection) {
+            $media = $media->merge($missionQuestion->getMedia($collection));
+        }
+
+        return \App\Http\Resources\Media\Resource::collection($media);
+    }
+
+    public function attachMedia(AttachMediaRequest $request, string $ulid): Resource
+    {
+        $validated = $request->validated();
+
+        $missionQuestion = MissionQuestion::query()
+            ->where('ulid', $ulid)
+            ->firstOrFail();
+
+        $signedURL = Storage::disk('azure_tmp')->url($validated['media_file_storage_path']);
+        $response = Http::get($signedURL);
+
+        $media = $missionQuestion
+            ->addMediaFromStream($response->body())
+            ->usingFileName(basename($validated['media_file_storage_path']))
+            ->withCustomProperties([
+                'member_ulid' => $validated['member_ulid'],
+            ])
+            ->toMediaCollection(
+                Arr::first(
+                    MissionQuestion::MEDIA_COLLECTIONS,
+                    fn ($collection) => $collection === $validated['collection']
+                )
+            );
+        // Delete from the temp disk and the main disk temp location
+        DeleteTemporaryFileJob::dispatch(
+            ['azure'],
+            $validated['media_file_storage_path'],
+        );
+
+        return new Resource($media);
+    }
+
+    public function deleteMedia(string $ulid, string $mediaUuid): JsonResponse
+    {
+        config('media-library.media_model')::query()
+            ->where('uuid', $mediaUuid)
+            ->delete();
+
+        return response()->json([
+            'message' => 'Deleted successfully.',
+        ], 204);
     }
 }
--- routes/api/v1.php
+++ routes/api/v1.php
@@ -266,6 +266,10 @@ Route::group([
     Route::get('/', [MissionQuestionController::class, 'index'])->name('index');
     Route::post('/', [MissionQuestionController::class, 'store'])->name('store');
     Route::match(['put', 'patch'], '/{ulid}', [MissionQuestionController::class, 'update'])->name('update');
+    Route::get('/{ulid}/media', [MissionQuestionController::class, 'getMedia'])->name('get-media');
+    Route::post('/{ulid}/media', [MissionQuestionController::class, 'attachMedia'])->name('attach-media');
+    Route::delete('/{ulid}/media/{mediaUuid}', [MissionQuestionController::class, 'deleteMedia'])->name('delete-media');
+
 });
 
 Route::group([
