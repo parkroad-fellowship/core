1f8c6ed5705b9f088a29ff520edf16917557db06
Miller Adulu <milleradulu@gmail.com>
2026-07-22 21:16:39 +0300

    Remove duplicate CSRF check

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -37,6 +37,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->validateCsrfTokens(except: [
             'broadcasting/*',
+            'telescope/*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
