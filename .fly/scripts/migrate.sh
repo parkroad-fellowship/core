afec5a961e58cc29cfa1b8dff9655ac85d157a64
Miller Adulu <milleradulu@gmail.com>
2026-08-06 01:46:14 +0300

    Properly seed tenant permissions when deploying

--- .fly/scripts/migrate.sh
+++ .fly/scripts/migrate.sh
@@ -2,5 +2,6 @@
 
 /usr/bin/php /var/www/html/artisan migrate --force --no-ansi
 /usr/bin/php /var/www/html/artisan db:seed --class=RolesAndPermissionsSeeder --force --no-ansi
-/usr/bin/php /var/www/html/artisan db:seed --class=LeadershipPermissionsSeeder --force --no-ansi
+/usr/bin/php /var/www/html/artisan tenants:seed --class=RolesAndPermissionsSeeder --force --no-ansi
+/usr/bin/php /var/www/html/artisan tenants:seed --class=LeadershipPermissionsSeeder --force --no-ansi
 # /usr/bin/php /var/www/html/artisan db:seed --class=ProductionSeeder --force --no-ansi: --- IGNORE: Meant to run once ---
