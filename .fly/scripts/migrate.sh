e2eea773646412956758962fd40062d76fa45b37
Miller Adulu <milleradulu@gmail.com>
2026-08-26 13:24:26 +0300

    Disable `LeadershipPermissionsSeeder`

--- .fly/scripts/migrate.sh
+++ .fly/scripts/migrate.sh
@@ -3,5 +3,5 @@
 /usr/bin/php /var/www/html/artisan migrate --force --no-ansi
 /usr/bin/php /var/www/html/artisan db:seed --class=RolesAndPermissionsSeeder --force --no-ansi
 /usr/bin/php /var/www/html/artisan tenants:seed --class=RolesAndPermissionsSeeder --force --no-ansi
-/usr/bin/php /var/www/html/artisan tenants:seed --class=LeadershipPermissionsSeeder --force --no-ansi
+# /usr/bin/php /var/www/html/artisan tenants:seed --class=LeadershipPermissionsSeeder --force --no-ansi
 # /usr/bin/php /var/www/html/artisan db:seed --class=ProductionSeeder --force --no-ansi: --- IGNORE: Meant to run once ---
