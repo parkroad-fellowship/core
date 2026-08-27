987084c8a70ea78551d09cc13989b768d7fab2e4
Miller Adulu <milleradulu@gmail.com>
2026-08-27 09:31:16 +0300

    Seed leadership roles on one tenant

--- .fly/scripts/migrate.sh
+++ .fly/scripts/migrate.sh
@@ -3,5 +3,5 @@
 /usr/bin/php /var/www/html/artisan migrate --force --no-ansi
 /usr/bin/php /var/www/html/artisan db:seed --class=RolesAndPermissionsSeeder --force --no-ansi
 /usr/bin/php /var/www/html/artisan tenants:seed --class=RolesAndPermissionsSeeder --force --no-ansi
-# /usr/bin/php /var/www/html/artisan tenants:seed --class=LeadershipPermissionsSeeder --force --no-ansi
+/usr/bin/php /var/www/html/artisan tenants:seed --class=LeadershipPermissionsSeeder --tenants=01kz9r08gfgb4v54k85rh8snmj --force --no-ansi
 # /usr/bin/php /var/www/html/artisan db:seed --class=ProductionSeeder --force --no-ansi: --- IGNORE: Meant to run once ---
