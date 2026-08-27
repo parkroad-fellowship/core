518458d471049e6678d38afffa0ac6e1ec90ca05
Miller Adulu <milleradulu@gmail.com>
2026-08-27 12:33:51 +0300

    Rework migration flows

--- .fly/scripts/caches.sh
+++ .fly/scripts/caches.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-/usr/bin/php /var/www/html/artisan optimize:clear --no-ansi
-/usr/bin/php /var/www/html/artisan pulse:restart --no-ansi
+
--- .fly/scripts/migrate.sh
+++ .fly/scripts/migrate.sh
@@ -5,3 +5,6 @@
 /usr/bin/php /var/www/html/artisan tenants:seed --class=RolesAndPermissionsSeeder --force --no-ansi
 /usr/bin/php /var/www/html/artisan tenants:seed --class=LeadershipPermissionsSeeder --tenants=01kyvqgepfqh10z3r8wmeq6rcz --force --no-ansi
 # /usr/bin/php /var/www/html/artisan db:seed --class=ProductionSeeder --force --no-ansi: --- IGNORE: Meant to run once ---
+
+/usr/bin/php /var/www/html/artisan optimize:cache --no-ansi
+/usr/bin/php /var/www/html/artisan pulse:restart --no-ansi
