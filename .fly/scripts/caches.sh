71f3b122662cd1e7d5f4546987653a16f4458e1b
Miller Adulu <milleradulu@gmail.com>
2026-08-27 12:24:51 +0300

    Clear all cache items

--- .fly/scripts/caches.sh
+++ .fly/scripts/caches.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-/usr/bin/php /var/www/html/artisan optimize --no-ansi
+/usr/bin/php /var/www/html/artisan optimize:clear --no-ansi
 /usr/bin/php /var/www/html/artisan pulse:restart --no-ansi
