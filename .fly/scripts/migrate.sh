3e916e61d198ad40e95539568766409c5cf8c75e
Miller Adulu <milleradulu@gmail.com>
2026-08-27 11:32:56 +0300

    Only run for PRF

--- .fly/scripts/migrate.sh
+++ .fly/scripts/migrate.sh
@@ -2,6 +2,6 @@
 
 /usr/bin/php /var/www/html/artisan migrate --force --no-ansi
 /usr/bin/php /var/www/html/artisan db:seed --class=RolesAndPermissionsSeeder --force --no-ansi
-/usr/bin/php /var/www/html/artisan tenants:seed --class=LeadershipPermissionsSeeder --tenants=01kz9r08gfgb4v54k85rh8snmj --force --no-ansi
 /usr/bin/php /var/www/html/artisan tenants:seed --class=RolesAndPermissionsSeeder --force --no-ansi
+/usr/bin/php /var/www/html/artisan tenants:seed --class=LeadershipPermissionsSeeder --tenants=01kyvqgepfqh10z3r8wmeq6rcz --force --no-ansi
 # /usr/bin/php /var/www/html/artisan db:seed --class=ProductionSeeder --force --no-ansi: --- IGNORE: Meant to run once ---
