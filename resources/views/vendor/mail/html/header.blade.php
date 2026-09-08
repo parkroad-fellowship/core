f87588edc3039cab124b3e94c85a2899eba35376
Miller Adulu <milleradulu@gmail.com>
2026-09-08 13:08:22 +0300

    Add default logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="{{ $tenantSettings->logoURL }}" class="logo" alt="{{ $tenantSettings->organizationName }}">
+        <img src="{{ $tenantSettings->logoURL ?? '/logo.png' }}" class="logo" alt="{{ $tenantSettings->organizationName }}">
         </a>
     </td>
 </tr>
