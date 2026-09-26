2bb61d2877d6e32c99ca32fc0d92d976987f5e43
Miller Adulu <milleradulu@gmail.com>
2026-09-26 12:01:53 +0300

    Hide payments

--- app/Filament/Resources/Payments/Pages/ListPayments.php
+++ app/Filament/Resources/Payments/Pages/ListPayments.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Payments\Pages;
 
 use App\Filament\Resources\Payments\PaymentResource;
+use App\Models\Payment;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
@@ -13,7 +14,13 @@ class ListPayments extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            CreateAction::make()->visible(userCan(Payment::permission('create'))),
         ];
+    }
+
+    public static function canAccess(array $parameters = []): bool
+    {
+        return false;
+        return userCan(Payment::permission('viewAny'));
     }
 }
